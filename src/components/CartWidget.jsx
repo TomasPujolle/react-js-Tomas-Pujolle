e66227c07f96ccc954190c9c582fa571a421d220
Tomas pujolle <alan_pujolle@hotmail.com>
2022-08-09 20:33:16 -0300

    Arreglo de cart

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,14 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useCartContext } from './CartContext';
+import "../Styles/CartWidgetStyles.css";
 
 
 const CartWidget = () => {
 
+    const { totalItems } = useCartContext();
     
 
     return (
-        <div>
-            <i className="bi bi-cart2"></i>
+        <div className='contenedor'>
+            <i className="bi bi-cart2 cart"></i>
+            <span className='Number'> {totalItems() || ""}</span>
         </div>
        
     );
